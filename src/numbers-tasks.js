3d569df33ae1eb9bddcca6caaa2bd1a34abb238c
oneight15 <oneight1508@gmail.com>
2024-11-24 15:46:16 +0500

    feat: add last solutions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -272,8 +272,20 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  const arr = [0, 1];
+  let firstNum = 0;
+  let secondNum = 1;
+  let fiboNum;
+  for (let i = 1; i < index; i += 1) {
+    fiboNum = firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = fiboNum;
+  }
+  if (arr.includes(index)) {
+    return arr[index];
+  }
+  return fiboNum;
 }
 
 /**
@@ -579,8 +591,8 @@ function getIntegerPartNumber(number) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+function getSumOfNumbers(x1, x2, x3) {
+  return +(x1 + x2 + x3).toFixed(2);
 }
 
 /**
@@ -611,8 +623,8 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  throw new Error('Not implemented');
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 /**
@@ -625,8 +637,8 @@ function getRandomInteger(/* min, max */) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
+function getHypotenuse(a, b) {
+  return Math.hypot(a, b);
 }
 
 /**
@@ -642,8 +654,22 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  let res = 0;
+  if (number >= 0) {
+    for (let i = 0; i <= number; i += 1) {
+      if (i % 2) {
+        res += 1;
+      }
+    }
+  } else {
+    for (let i = 0; i >= number; i -= 1) {
+      if (i % 2) {
+        res += 1;
+      }
+    }
+  }
+  return res;
 }
 
 module.exports = {
